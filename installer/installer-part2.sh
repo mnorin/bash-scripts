b73166c8f12cc375c60b30c833a35ca9b7ec6b7f
Maxim Norin <maxim.norin@gmail.com>
2016-11-25 16:25:37 +1300

    rmoved unnecessary installer template

--- installer/installer-part2.sh
+++
@@ -1,9 +0,0 @@
-EOF
-
-echo -n "Decoding..."
-base64 -d /tmp/archive.base > /tmp/archive.tar.bz2
-echo "Done"
-
-echo -n "Extracting..."
-tar xf /tmp/archive.tar.bz2 --strip=3 -C $INSTALLATION_PATH
-echo "Done"
--- installer/make-installer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat installer-part1.sh archive.base installer-part2.sh > installer.sh
-chmod +x installer.sh
