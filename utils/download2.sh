aac80ad68b31de58d2a8338d671f89bb77f42e10
Maxim Norin <maxim.norin@gmail.com>
2018-12-10 08:47:18 +1300

    Changed variable PATH name
    Changed variable PATH name to avoid overlapping the environment variable

--- utils/download2.sh
+++ utils/download2.sh
@@ -12,14 +12,14 @@ HOST=${URL/$PROTOCOL/}
 HOST=${HOST%%/*}
 FILENAME=${URL##*/}
 SAVENAME=${FILENAME:-result}
-PATH=${URL/$PROTOCOL/}
-PATH=${PATH/$HOST/}
-PATH=${PATH/$FILENAME/}
+URLPATH=${URL/$PROTOCOL/}
+URLPATH=${URLPATH/$HOST/}
+URLPATH=${URLPATH/$FILENAME/}
 PORT=80
 HEADERS="HTTP/1.1\r\nHost: $HOST\r\nConnection: close\r\nContent-Length: 0\r\n\r\n"
 
 exec 3<>/dev/tcp/$HOST/$PORT
-echo -e "GET $PATH$FILENAME $HEADERS" >&3
-/bin/cat <&3 > $SAVENAME
+echo -e "GET $URLPATH$FILENAME $HEADERS" >&3
+cat <&3 > $SAVENAME
 
-/bin/sed -i $SAVENAME -e '1,/^\r$/d'
+sed -i $SAVENAME -e '1,/^\r$/d'
